43fcc43ed4ad28e692057398db792775d7c1e773
Laney Black <laney.black.74@gmail.com>
2019-03-16 10:56:45 +0300

    Fixed pom.xml

--- EduRate/src/main/java/com/bits/squad/edurate/ClientCode1.java
+++ EduRate/src/main/java/com/bits/squad/edurate/ClientCode1.java
@@ -1,18 +1,10 @@
 package com.bits.squad.edurate;
 
 import com.bits.squad.edurate.model.User;
-import com.bits.squad.edurate.util.HibernateUtil;
 import org.hibernate.*;
 
 public class ClientCode1 {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
 
-        session.beginTransaction();
-
-        User user = new User("Anton001", "123456");
-
-        session.save(user);
-        session.getTransaction().commit();
     }
 }
