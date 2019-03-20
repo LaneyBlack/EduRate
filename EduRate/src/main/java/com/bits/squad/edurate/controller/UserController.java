bd9db3680a8f4d21596f5a801e29962c14333bd5
Laney Black <laney.black.74@gmail.com>
2019-03-20 19:49:25 +0300

    Changed UserController and home.jsp

--- EduRate/src/main/java/com/bits/squad/edurate/controller/UserController.java
+++ EduRate/src/main/java/com/bits/squad/edurate/controller/UserController.java
@@ -2,6 +2,7 @@ package com.bits.squad.edurate.controller;
 
 import com.bits.squad.edurate.model.User;
 import com.bits.squad.edurate.service.UserService;
+import com.bits.squad.edurate.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,5 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userServiceImpl;
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public void registerUser(String username, String password) {
+        User user = new User(username, password);
+        userServiceImpl.saveUser(user);
+    }
 }
