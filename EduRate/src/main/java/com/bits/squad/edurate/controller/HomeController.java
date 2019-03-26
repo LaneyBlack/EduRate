a7b9b37f1eef20caef907a5135d7776a37a0bc7e
Laney Black <laney.black.74@gmail.com>
2019-03-26 13:37:24 +0300

    Changed HomeController

--- EduRate/src/main/java/com/bits/squad/edurate/controller/HomeController.java
+++ EduRate/src/main/java/com/bits/squad/edurate/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bits.squad.edurate.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,8 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@Controller
 public class HomeController {
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView homeStartMethod(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView("home");
